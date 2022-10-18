c7137a2dca404d62c665086de138fc3783ec9a08
Antonin Faure <antoninfaure@tsf-484-wpa-4-209.epfl.ch>
2022-10-18 14:49:29 +0200

    added implementations

--- implementations.py
+++ implementations.py
@@ -61,7 +61,7 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     w = initial_w.copy()
     for it in range(max_iters):
         loss = np.sum(np.logaddexp(0, tx @ w) - y * tx.dot(w))/tx.shape[0]
-        grad = tx.T.dot(sigmoid(tx.dot(w)) - y) + 2*lambda_*w
+        grad = (tx.T.dot(sigmoid(tx.dot(w)) - y))/tx.shape[0] + 2*lambda_*w
         w -= gamma * grad
         # log info
         if it % 100 == 0:
@@ -75,3 +75,35 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     loss = np.sum(np.logaddexp(0, tx @ w) - y * tx.dot(w))/tx.shape[0]
     print("loss={l}".format(l=loss))
     return (w, loss)
+
+def least_squares(y, tx):
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+    
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        loss: scalar.
+
+    """
+    #We solve the normal equations using QR decomposition, which is a computationally efficient method.
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a, b)
+    err = y-tx.dot(w)
+    loss =  1/2*np.mean(err**2)
+    
+    return w, loss
+
+def ridge_regression(y, tx, lambda_):
+    A = tx.T.dot(tx) + 2*tx.shape[0]*lambda_*np.identity(tx.shape[1])
+    B = tx.T.dot(y)
+    w = np.linalg.solve(A,B)
+    loss = 1/(2*tx.shape[0])*(np.sum((y - tx.dot(w))**2)+lambda_*np.sum(w**2))
+
+    print(w)
+    print(loss)
+    return w, loss
