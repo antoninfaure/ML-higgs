674dcd900da6d207e6f9d64fff0a688f7fafafd7
mdorster <115158974+mdorster@users.noreply.github.com>
2022-10-24 18:04:30 +0200

    Add files via upload

---
+++ helpers (4).py
@@ -0,0 +1,82 @@
+import numpy as np
+import csv as csv
+
+def load_data(path_dataset,sub_sample=True, add_outlier=False, train=True):
+    """Load data and convert it to the metric system."""
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", dtype=str,  skip_header=1)
+    ids = data[:,0]
+    labels = data[:,1]
+    if train == True:
+        labels[labels=='s']=1
+        labels[labels=='b']=-1
+        labels = np.asarray(labels, dtype=float)
+    data = np.delete(data, [0,1], 1)
+    data = np.asarray(data, dtype=float)
+    return data, labels, ids
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in .csv format for submission to Kaggle or AIcrowd
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, "w") as csvfile:
+        fieldnames = ["Id", "Prediction"]
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({"Id": int(r1), "Prediction": int(r2)})
+
+
+# Shuffle data
+def shuffle_data(y, tx, seed=1):
+    np.random.seed(seed)
+    inds = np.random.permutation(tx.shape[0])
+    tx = tx[inds]
+    y = y[inds]
+    return y, tx
+
+# Slice data and labels into training and validation sets
+def slice_data(y, tx, ratio, seed=1): 
+    slice_id = int(np.floor(y.shape[0]*ratio))
+    y_va, y_tr = y[:slice_id], y[slice_id:]
+    tx_va, tx_tr = tx[:slice_id], tx[slice_id:]
+    return y_va, y_tr, tx_va, tx_tr
+
+def clean_data(data):
+    # Replace -999 by nan
+    data = np.where(data == -999, np.nan, data)
+    # Compute the columns means without nan values 
+    means = np.nanmean(data, axis=0)
+    #Find indices that you need to replace
+    inds = np.where(np.isnan(data))
+    #Place column means in the indices. Align the arrays using take
+    data[inds] = np.take(means, inds[1])
+    return data
+
+# Standardize the data
+def standardize(x):
+    #Standardize the original data set.
+    mean_x = np.mean(x, axis=0)
+    x = x - mean_x
+    std_x = np.std(x, axis=0)
+    x = x / std_x
+    return x
+
+def predict_logistic(tx, w):
+    def sigmoid(t):
+        return 1.0 / (1 + np.exp(-t))
+    y = sigmoid(tx @ w)
+    # s = 1 , b = -1
+    y[y > 0.5] = 1
+    y[y <= 0.5] = -1
+    return y
+
+def accuracy(a, b):
+    return np.sum(a == b)/a.shape[0]
+
+def initialize_weights(tx):
+    initial_w = np.random.normal(0., 0.1, [tx.shape[1],])
+    return initial_w
---
+++ hyperparameter_opti.py
@@ -0,0 +1,91 @@
+import implementations as impl
+import helpers as helpers
+import numpy as np
+
+def best_lambda_selection(y, tx, max_iters, gamma):
+	seed = 12
+	lambdas = np.logspace(-6, 1, 3) #change last parameter to 10 if it works
+
+	# Split data into training and validation sets
+	y_va, y_tr, tx_va, tx_tr = helpers.slice_data(y, tx, 0.25, seed)
+
+	# Add biases to data
+	tx_va = np.c_[np.ones((y_va.shape[0], 1)), tx_va]
+	tx_tr = np.c_[np.ones((y_tr.shape[0], 1)), tx_tr]
+
+	initial_w = np.random.normal(0., 0.1, [tx_tr.shape[1],])
+
+	# define lists to store the loss of training data and validation data
+	losses_training = []
+	losses_validation = []
+
+	# cross validation
+	for lambda_ in lambdas:
+		print(f"Current lambda={lambda_}")
+
+		# weights and training loss for logistic regression model:
+		w, loss_training = impl.reg_logistic_regression(y_tr, tx_tr, lambda_, initial_w, max_iters, gamma) 
+
+		# calculate the loss for validation data:
+		loss_validation = (np.sum(np.logaddexp(0, tx_va.dot(w)) + y_va * tx_va.dot(w)) + lambda_*np.linalg.norm(w)**2)/y_va.shape[0]
+
+		losses_training.append(loss_training)
+		losses_validation.append(loss_validation)
+		print(f"training_loss = {loss_training}, validation_loss = {loss_validation}")
+
+	ind_best_lambda = np.argmin(losses_validation)
+	best_lambda = lambdas[ind_best_lambda]
+	print(f"Best lambda = {best_lambda}, training_loss = {losses_training[ind_best_lambda]}, validation_loss = {losses_validation[ind_best_lambda]}")
+	return best_lambda
+
+def best_gamma_selection(y, tx, max_iters):
+	seed = 12
+	gammas = np.logspace(-6, 1, 9) #use linspace maybe to go up to 2
+
+	# Split data into training and validation sets
+	y_va, y_tr, tx_va, tx_tr = helpers.slice_data(y, tx, 0.25, seed)
+
+	# Add biases to data
+	tx_va = np.c_[np.ones((y_va.shape[0], 1)), tx_va]
+	tx_tr = np.c_[np.ones((y_tr.shape[0], 1)), tx_tr]
+
+	initial_w = np.random.normal(0., 0.1, [tx_tr.shape[1],])
+
+	# define lists to store the loss of training data and validation data
+	losses_training = []
+	losses_validation = []
+
+	for gamma in gammas:
+		print(f"Current gamma={gamma}")
+
+		w, loss_training = impl.logistic_regression(y_tr, tx_tr, initial_w, max_iters, gamma=gamma)
+
+		# calculate the loss for validation data:
+		loss_validation = np.sum(np.logaddexp(0, tx_va.dot(w)) + y_va * tx_va.dot(w))/y_va.shape[0]
+
+		losses_training.append(loss_training)
+		losses_validation.append(loss_validation)
+		print(f"training_loss = {loss_training}, validation_loss = {loss_validation}")
+
+	ind_best_gamma = np.argmin(losses_validation)
+	best_gamma = gammas[ind_best_gamma]
+	print(f"Best gamma = {best_gamma}, training_loss = {losses_training[ind_best_gamma]}, validation_loss = {losses_validation[ind_best_gamma]}")
+	return best_gamma
+
+
+
+# Load train data
+#tx, y, ids = helpers.load_data('train.csv')
+
+# Clean and standardize train data
+#tx = helpers.standardize(helpers.clean_data(tx))
+#y[y==-1]=0
+
+# Shuffle data
+#y, tx = helpers.shuffle_data(y, tx)
+
+#best_gamma = best_gamma_selection(y, tx, 1000)
+
+#best_lambda = best_lambda_selection(y, tx, 1000, best_gamma)
+
+#exit()
